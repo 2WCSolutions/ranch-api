059b92806bbbedf4d7eea97676c0c191e61443de
William Cobb <william@2wc.com>
2022-11-11 12:00:46 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { json } from 'body-parser';
+// import { postRouter } from './routes/post.routes'
+
 console.log('Hello world!')
