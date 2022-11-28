7d4f2801f60cfb3bbc5d885dd9d459af70255eee
William Cobb <william@2wc.com>
2022-11-28 06:58:33 -0500

    update

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -3,19 +3,21 @@ import Controller from '../interfaces/controller.interface';
 import { Image } from '../models/image.model';
 import { Buffer } from 'node:buffer';
 import { BlobServiceClient, ContainerClient } from '@azure/storage-blob';
+import { Constants } from '../libs/constants';
 
 const fs = require('fs');
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(
-    "BlobEndpoint=https://ranchstorage.blob.core.windows.net;SharedAccessSignature=sp=racwdl&st=2022-11-25T19:10:10Z&se=2029-11-26T03:10:10Z&spr=https&sv=2021-06-08&sr=c&sig=COvLdIVRfT98kAFeXFl1u4pCk%2B63z6y1EEXpMWb3ajY%3D"
+  "BlobEndpoint=https://ranchstorage.blob.core.windows.net;SharedAccessSignature=sp=racwdl&st=2022-11-25T19:10:10Z&se=2029-11-26T03:10:10Z&spr=https&sv=2021-06-08&sr=c&sig=COvLdIVRfT98kAFeXFl1u4pCk%2B63z6y1EEXpMWb3ajY%3D"
 );
-  const containerClient = blobServiceClient.getContainerClient(
-    "media"
+const containerClient = blobServiceClient.getContainerClient(
+  "media"
 );
 
 
 class ImageController implements Controller {
-    public path = '/images';
+
+    public path = Constants.PATH_IMAGES;
     public router = Router();
    
     public getAllImages = async (request: Request, response: Response) => {
@@ -75,15 +77,43 @@ class ImageController implements Controller {
       return response.status(201).send();
     }
 
-
-
+    uploadDocumentToAzure = async (imageData: String) => {
+      console.log("test 1");
+  
+      console.log("test 1");
+              const accountName = process.env.AZURE_STORAGE_ACCOUNT;
+              const sasToken = process.env.AZURE_STORAGE_SASTOKEN;
+              if (!accountName) throw Error('Azure Storage accountName not found');
+              if (!sasToken) throw Error('Azure Storage accountKey not found');
+      console.log("test 2");
+      
+              const blobServiceUri = process.env.AZURE_STORAGE_BLOB_URL;
+      
+      
+     const containerClient: ContainerClient = blobServiceClient.getContainerClient("media");
+     console.log("test 3");
+  
+      const data = Buffer.from(imageData, "base64");
+      const blockBlobClient = containerClient.getBlockBlobClient("test4.jpg");
+      const response = await blockBlobClient.uploadData(data, {
+        blobHTTPHeaders: {
+          
+        },
+      });
+      if (response._response.status !== 201) {
+        throw new Error(
+          `Error uploading document ${blockBlobClient.name} to container ${blockBlobClient.containerName}`
+        );
+      }
+      console.log("test 4");
+    };
+  
     
-
+/*
     download = async () => {
 
         // connect-with-sas-token.js
         const { BlobServiceClient } = require('@azure/storage-blob');
-console.log("test 1");
         const accountName = "ranchstorage";
         const sasToken = "sp=racwdl&st=2022-11-25T19:10:10Z&se=2029-11-26T03:10:10Z&spr=https&sv=2021-06-08&sr=c&sig=COvLdIVRfT98kAFeXFl1u4pCk%2B63z6y1EEXpMWb3ajY%3D";
         if (!accountName) throw Error('Azure Storage accountName not found');
@@ -115,116 +145,9 @@ console.log("test 4");
         
 
     }
-
-
-  uploadDocumentToAzure = async (imageData: String) => {
-    console.log("test 1");
-
-    console.log("test 1");
-            const accountName = "ranchstorage";
-            const sasToken = "sp=racwdl&st=2022-11-25T19:10:10Z&se=2029-11-26T03:10:10Z&spr=https&sv=2021-06-08&sr=c&sig=COvLdIVRfT98kAFeXFl1u4pCk%2B63z6y1EEXpMWb3ajY%3D";
-            if (!accountName) throw Error('Azure Storage accountName not found');
-            if (!sasToken) throw Error('Azure Storage accountKey not found');
-    console.log("test 2");
-    
-            const blobServiceUri = `https://${accountName}.blob.core.windows.net`;
-    
-    
-   const containerClient: ContainerClient = blobServiceClient.getContainerClient("media");
-   console.log("test 3");
-
-    const data = Buffer.from(imageData, "base64");
-    const blockBlobClient = containerClient.getBlockBlobClient("test3.jpg");
-    const response = await blockBlobClient.uploadData(data, {
-      blobHTTPHeaders: {
-        
-      },
-    });
-    if (response._response.status !== 201) {
-      throw new Error(
-        `Error uploading document ${blockBlobClient.name} to container ${blockBlobClient.containerName}`
-      );
-    }
-    console.log("test 4");
-  };
-
-
+*/
 
   }
 
-//   const uploadDocumentToAzure = async () => {
-//     const data = Buffer.from("BASE-64-ENCODED-PDF", "base64");
-//     const blockBlobClient = containerClient.getBlockBlobClient("FILENAME-TO-UPLOAD");
-//     const response = await blockBlobClient.uploadData(data, {
-//       blobHTTPHeaders: {
-//         blobContentType: "application/pdf",
-//       },
-//     });
-//     if (response._response.status !== 201) {
-//       throw new Error(
-//         `Error uploading document ${blockBlobClient.name} to container ${blockBlobClient.containerName}`
-//       );
-//     }
-//   };
-
-//   const uploadFileToBlob = async (file: Blob | null): Promise<string[]> => {
-//     if (!file) return [];
-
-//     const containerName = `media`;
-//     const sasToken = "sp=racwdl&st=2022-11-25T19:10:10Z&se=2029-11-26T03:10:10Z&spr=https&sv=2021-06-08&sr=c&sig=COvLdIVRfT98kAFeXFl1u4pCk%2B63z6y1EEXpMWb3ajY%3D";
-//     const storageAccountName = "ranchstorage";
-
-  
-//     // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
-//     const blobService = new BlobServiceClient(
-//       `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
-//     );
-  
-//     // get Container - full public read access
-//     const containerClient: ContainerClient = blobService.getContainerClient(containerName);
-//     await containerClient.createIfNotExists({
-//       access: 'container',
-//     });
-  
-//     // upload file
-//     await createBlobInContainer(containerClient, file);
-  
-//     // get list of blobs in container
-//     return getBlobsInContainer(containerClient);
-//   };
-
-
-
-//   const createBlobInContainer = async (containerClient: ContainerClient, file: Blob) => {
-  
-//     // create blobClient for container
-//     const blobClient = containerClient.getBlockBlobClient("aaTrestereresaas.jpg");
-  
-//     // set mimetype as determined from browser with file upload control
-//     const options = { blobHTTPHeaders: { blobContentType: file.type } };
-  
-//     // upload file
-//     await blobClient.uploadData(file, options);
-//   }
-  
-//   // return list of blobs in container to display
-// const getBlobsInContainer = async (containerClient: ContainerClient) => {
-//   const returnedBlobUrls: string[] = [];
-
-//   const containerName = `media`;
-//   const sasToken = "sp=racwdl&st=2022-11-25T19:10:10Z&se=2029-11-26T03:10:10Z&spr=https&sv=2021-06-08&sr=c&sig=COvLdIVRfT98kAFeXFl1u4pCk%2B63z6y1EEXpMWb3ajY%3D";
-//   const storageAccountName = "ranchstorage";
-
-//   // get list of blobs in container
-//   // eslint-disable-next-line
-//   for await (const blob of containerClient.listBlobsFlat()) {
-//     // if image is public, just construct URL
-//     returnedBlobUrls.push(
-//       `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
-//     );
-//   }
-
-//   return returnedBlobUrls;
-// } 
   export default ImageController;
   
---
+++ src/libs/constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+    public static PATH_IMAGES: string = "/images";  
+}
+
